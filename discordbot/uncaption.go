831ff7d138d15b41aa4eee91c47f15d1eb6caa10
Samuel Hernandez <sam@samhza.com>
2024-01-08 10:09:02 -0500

    refactor uncaption

--- discordbot/uncaption.go
+++ discordbot/uncaption.go
@@ -12,11 +12,10 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/diamondburned/arikawa/v3/discord"
 	"github.com/diamondburned/arikawa/v3/gateway"
 	ff "samhza.com/ffmpeg"
 )
-
-// TODO: split Uncaption into multiple functions
 
 func (bot *Bot) Uncaption(m *gateway.MessageCreateEvent) error {
 	media, err := bot.findMedia(m.Message)
@@ -30,73 +29,53 @@ func (bot *Bot) Uncaption(m *gateway.MessageCreateEvent) error {
 	done := bot.startWorking(m.ChannelID, m.ID)
 	defer done()
 	defer resp.Body.Close()
+	var r io.ReadCloser
+	var ext string
 	switch media.Type {
 	case mediaImage:
-		im, _, err := image.Decode(resp.Body)
-		resp.Body.Close()
-		if err != nil {
-			return err
-		}
-		newMinY, found := detectCaption(im)
-		if !found {
-			return errors.New("couldn't find the caption")
-		}
-		b := im.Bounds()
-		b.Min.Y = newMinY
-		cropped := cropImage(im, b)
-		r, w := io.Pipe()
-		defer r.Close()
-		go func() {
-			png.Encode(w, cropped)
-			w.Close()
-		}()
-		done()
-		return bot.sendFile(m.ChannelID, m.ID, "uncaption", ".png", r)
+		r, err = bot.uncaptionImage(resp.Body)
+		ext = ".png"
 	case mediaGIF:
-		dgif, err := gif.DecodeAll(resp.Body)
-		resp.Body.Close()
+		r, err = bot.uncaptionGif(resp.Body)
+		ext = ".gif"
+	default:
+		body := resp.Body
+		out, err := bot.uncaptionVideo(body, media, m.ID)
 		if err != nil {
 			return err
 		}
-		newMinY, found := detectCaption(dgif.Image[0])
-		if !found {
-			return errors.New("couldn't find the caption")
-		}
-		bounds := dgif.Image[0].Rect
-		bounds.Min.Y = newMinY
-		for i, img := range dgif.Image {
-			dgif.Image[i] = img.SubImage(bounds).(*image.Paletted)
-			dgif.Image[i].Rect.Min.Y = 0
-			dgif.Image[i].Rect.Max.Y -= newMinY
-		}
-		dgif.Config.Height -= newMinY
-		r, w := io.Pipe()
-		go func() {
-			w.CloseWithError(gif.EncodeAll(w, dgif))
-		}()
 		done()
-		return bot.sendFile(m.ChannelID, m.ID, "uncaption", ".gif", r)
+		return out.Send(bot.Ctx.Client, m.ChannelID)
 	}
-	in, err := downloadInput(resp.Body)
-	resp.Body.Close()
 	if err != nil {
 		return err
+	}
+	done()
+	defer r.Close()
+	return bot.sendFile(m.ChannelID, m.ID, "uncaption", ext, r)
+}
+
+func (bot *Bot) uncaptionVideo(body io.ReadCloser, media *Media, mid discord.MessageID) (*outputFile, error) {
+	in, err := downloadInput(body)
+	body.Close()
+	if err != nil {
+		return nil, err
 	}
 	defer os.Remove(in.Name())
 	defer in.Close()
-	firstFrame, err := firstVidFrame(in.Name())
+	firstFrame, err := firstFrame(in.Name())
 	if err != nil {
-		return err
+		return nil, err
 	}
 	newMinY, found := detectCaption(firstFrame)
 	if !found {
-		return errors.New("couldn't find the caption")
+		return nil, errors.New("couldn't find the caption")
 	}
 	var instream ff.Stream = ff.InputFile{File: in}
 
 	probed, err := ff.Probe(in.Name())
 	if err != nil {
-		return err
+		return nil, err
 	}
 	var hasAudio bool
 	for _, stream := range probed.Streams {
@@ -123,21 +102,66 @@ func (bot *Bot) Uncaption(m *gateway.MessageCreateEvent) error {
 			streams = append(streams, ff.Audio(instream))
 		}
 	}
-	out, err := bot.createOutput(m.ID, "uncaption", "."+outfmt)
+	out, err := bot.createOutput(mid, "uncaption", "."+outfmt)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	fcmd := new(ff.Cmd)
 	fcmd.AddFileOutput(out.File, []string{"-y", "-f", outfmt}, streams...)
 	err = fcmd.Cmd().Run()
 	if err != nil {
-		return err
+		return nil, err
 	}
-	done()
-	return out.Send(bot.Ctx.Client, m.ChannelID)
+	return out, nil
 }
 
-func firstVidFrame(filename string) (image.Image, error) {
+func (*Bot) uncaptionGif(body io.ReadCloser) (io.ReadCloser, error) {
+	dgif, err := gif.DecodeAll(body)
+	body.Close()
+	if err != nil {
+		return nil, err
+	}
+	newMinY, found := detectCaption(dgif.Image[0])
+	if !found {
+		return nil, errors.New("couldn't find the caption")
+	}
+	bounds := dgif.Image[0].Rect
+	bounds.Min.Y = newMinY
+	for i, img := range dgif.Image {
+		dgif.Image[i] = img.SubImage(bounds).(*image.Paletted)
+		dgif.Image[i].Rect.Min.Y = 0
+		dgif.Image[i].Rect.Max.Y -= newMinY
+	}
+	dgif.Config.Height -= newMinY
+	r, w := io.Pipe()
+	go func() {
+		w.CloseWithError(gif.EncodeAll(w, dgif))
+	}()
+	return r, nil
+}
+
+func (*Bot) uncaptionImage(resp io.ReadCloser) (io.ReadCloser, error) {
+	im, _, err := image.Decode(resp)
+	resp.Close()
+	if err != nil {
+		return nil, err
+	}
+	newMinY, found := detectCaption(im)
+	if !found {
+		return nil, errors.New("couldn't find the caption")
+	}
+	b := im.Bounds()
+	b.Min.Y = newMinY
+	cropped := cropImage(im, b)
+	r, w := io.Pipe()
+	go func() {
+		png.Encode(w, cropped)
+		w.Close()
+	}()
+	return r, nil
+}
+
+func firstFrame(filename string) (image.Image, error) {
 	var v ff.Stream = ff.Input{Name: filename}
 	v = ff.Filter(v, "select=eq(n\\,0)")
 	cmd := new(ff.Cmd)
