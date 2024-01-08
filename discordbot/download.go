18e522b08e12964c89bab3fbc1edd6bb06382eff
Samuel Hernandez <sam@samhza.com>
2024-01-07 23:47:20 -0500

    use yt-dlp via command line in download command

--- discordbot/download.go
+++ discordbot/download.go
@@ -20,20 +20,32 @@ var downloadScript []byte
 func (b *Bot) Download(m *gateway.MessageCreateEvent, args bot.RawArguments) error {
 	done := b.startWorking(m.ChannelID, m.ID)
 	defer done()
-	cmd := exec.Command("python3", "-", string(args))
+	dir, err := os.MkdirTemp("", "esammy")
+	defer os.RemoveAll(dir)
+	cmd := exec.Command(
+		"yt-dlp",
+		"--no-playlist",
+		"--max-filesize", "500m",
+		"--print", "filename",
+		"--no-simulate",
+		"-S", "mp4",
+		"--match-filter", "duration <=? 600 & !was_live & !is_live",
+		"--output", filepath.Join(dir, "%(title)s %(id)s.%(ext)s"),
+		"--",
+		string(args))
 	stderr := strings.Builder{}
 	cmd.Stdin = bytes.NewReader(downloadScript)
 	cmd.Stderr = &stderr
 	out, err := cmd.Output()
-	if err != nil {
+	if err != nil || len(out) == 0 {
 		var exitError *exec.ExitError
-		if errors.As(err, &exitError) {
-			return fmt.Errorf("exit status %d: %s",
-				exitError.ExitCode(), string(stderr.String()))
+		if err == nil || errors.As(err, &exitError) {
+			return fmt.Errorf(string(stderr.String()))
 		}
 		return err
 	}
 	outfile := string(out[:len(out)-1])
+	_, basename := filepath.Split(outfile)
 	ext := filepath.Ext(outfile)
 	f, err := os.Open(outfile)
 	if err != nil {
@@ -42,7 +54,8 @@ func (b *Bot) Download(m *gateway.MessageCreateEvent, args bot.RawArguments) err
 	of := new(outputFile)
 	of.File = f
 	of.mid = m.ID
-	of.name = m.ID.String() + "." + ext
+	of.name = basename[:len(basename)-len(ext)]
+	of.ext = ext
 	of.bot = b
 	return of.Send(b.Ctx.Client, m.ChannelID)
 }
